c8529682c94dff64261ee7f11dc97ba37ae492e3
Nils von Däniken <nils.vondaeniken@@lernende.bbw.ch>
2023-03-25 23:14:33 +0100

    Ticket siite die erste Schritt gmacht

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import Home from "./components/home/home";
+import TicketKauf from "./components/home/ticketKauf";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Link to={'/home'}>Home</Link>
+                <ul>
+                    <li><Link to={'/home'}>Home</Link></li>
+                    <li><Link to={'/ticketKauf'}>Ticket Kauf</Link></li>
+                </ul>
+
                 <Routes>
                     <Route path={'home'} element={<Home/>}/>
+                    <Route path={'ticketKauf'} element={<TicketKauf/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ src/components/home/ticketKauf.js
@@ -0,0 +1,35 @@
+import "./ticketKauf.css"
+function TicketKauf(){
+    return(
+        <div>
+        <header>
+            <hr></hr>
+            <h1>Tickets</h1>
+            <hr></hr>
+        </header>
+        <body>
+        <div>
+            <h2>Zoo</h2>
+            <details>
+                <summary>
+                    Ticket 1
+                </summary>
+                <img src="../../images/img.png"/>
+            </details>
+            <hr></hr>
+        </div>
+        <div>
+            <h2>Restaurant</h2>
+            <details>
+                <summary>
+                    Ticket 2
+                </summary>
+                TICKETS
+            </details>
+            <hr></hr>
+        </div>
+        </body>
+        </div>
+    );
+}
+export default TicketKauf
