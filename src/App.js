7db52a5fcef70f0b691c5189898c2ad2f8e6786d
Colin <colin.marti@lernende.bbw.ch>
2023-04-03 13:24:55 +0200

    app bar implemented

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ import HomeIcon from '@mui/icons-material/Home';
 import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
 import {useState} from "react";
+import LockIcon from '@mui/icons-material/Lock';
+import Map from "./components/map/map";
+import Security from "./components/security/security";
+import Savanne from "./components/home/savanne";
 
 function App() {
     const [value, setValue] = useState(0);
 
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
     return (
         <div className="App">
             <BrowserRouter>
@@ -18,19 +25,22 @@ function App() {
                     <BottomNavigation
                         showLabels
                         value={value}
-                        onChange={(event, newValue) => {
-                            setValue(newValue);
-                        }}>
+                        onChange={handleChange}>
                         <BottomNavigationAction component={Link} to={'/ticket'} label="Tickets"
                                                 icon={<ConfirmationNumberIcon/>}/>
                         <BottomNavigationAction component={Link} to={'/home'} label="Home" icon={<HomeIcon/>}/>
                         <BottomNavigationAction component={Link} to={'/map'} label="Map" icon={<FmdGoodIcon/>}/>
+                        <BottomNavigationAction component={Link} to={'/security'} label="Datenschutz" icon={<LockIcon/>}/>
                     </BottomNavigation>
                 </Paper>
 
                 <Routes>
-                    <Route path={'home'} element={<Home/>}/>
-                    <Route path={'ticket'} element={<TicketKauf/>}/>
+                    <Route path={'/ticket'} element={<TicketKauf/>}/>
+                    <Route path={'/home'} element={<Home/>}/>
+                    <Route path={'/home/savanne'} element={<Savanne/>}/>
+                    <Route path={'/map'} element={<Map/>}/>
+                    <Route path={'/security'} element={<Security/>}/>
+
                 </Routes>
             </BrowserRouter>
         </div>
