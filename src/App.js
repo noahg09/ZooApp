f3f5950ee1eff1b846b6af8e3eb12d00ca6dfe03
Colin <colin.marti@lernende.bbw.ch>
2023-04-03 08:16:01 +0200

    app bar implemented

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import './App.css';
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import Home from "./components/home/home";
 import TicketKauf from "./components/ticketKauf/ticketKauf";
+import {BottomNavigation, BottomNavigationAction, Paper} from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <ul>
-                    <li><Link to={'/home'}>Home</Link></li>
-                    <li><Link to={'/ticketKauf'}>Ticket Kauf</Link></li>
-                </ul>
+                <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+                    <BottomNavigation showLabels>
+                        <BottomNavigationAction component={Link} to={'/ticket'} label="Tickets" icon={<ConfirmationNumberIcon />} />
+                        <BottomNavigationAction component={Link} to={'/home'} label="Home" icon={<HomeIcon />} />
+                        <BottomNavigationAction component={Link} to={'/map'} label="Map" icon={<FmdGoodIcon />} />
+                    </BottomNavigation>
+                </Paper>
 
                 <Routes>
                     <Route path={'home'} element={<Home/>}/>
-                    <Route path={'ticketKauf'} element={<TicketKauf/>}/>
+                    <Route path={'ticket'} element={<TicketKauf/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
