2f6b792374e41c80366607ca03f9f863c5a9c360
Colin <colin.marti@lernende.bbw.ch>
2023-04-03 08:19:21 +0200

    app bar implemented

--- src/App.js
+++ src/App.js
@@ -6,16 +6,25 @@ import {BottomNavigation, BottomNavigationAction, Paper} from "@mui/material";
 import HomeIcon from '@mui/icons-material/Home';
 import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import {useState} from "react";
 
 function App() {
+    const [value, setValue] = useState(0);
+
     return (
         <div className="App">
             <BrowserRouter>
-                <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-                    <BottomNavigation showLabels>
-                        <BottomNavigationAction component={Link} to={'/ticket'} label="Tickets" icon={<ConfirmationNumberIcon />} />
-                        <BottomNavigationAction component={Link} to={'/home'} label="Home" icon={<HomeIcon />} />
-                        <BottomNavigationAction component={Link} to={'/map'} label="Map" icon={<FmdGoodIcon />} />
+                <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={3}>
+                    <BottomNavigation
+                        showLabels
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                        }}>
+                        <BottomNavigationAction component={Link} to={'/ticket'} label="Tickets"
+                                                icon={<ConfirmationNumberIcon/>}/>
+                        <BottomNavigationAction component={Link} to={'/home'} label="Home" icon={<HomeIcon/>}/>
+                        <BottomNavigationAction component={Link} to={'/map'} label="Map" icon={<FmdGoodIcon/>}/>
                     </BottomNavigation>
                 </Paper>
 
