3d3e84fbf14f9f6a8dde88952164a4933e471c9f
noahg09 <noah.gruenig@hispeed.ch>
2023-04-17 15:43:39 +0200

    Savanne started

--- src/components/home/savanne.js
+++ src/components/home/savanne.js
@@ -1,9 +1,27 @@
+import Header from "../header/header";
+import {Divider, ImageList} from "@mui/material";
+import zebraImg from "../../images/zebra.png";
+import giraffeImg from "../../images/giraffeSavanne.png";
+import gazelleImg from "../../images/gazelle.png";
+import nashornImg from "../../images/nashorn.png";
 
 function Savanne() {
+    const titleValue = "Savanne";
 
     return(
         <div>
-            SAVANNE
+            <Header titleValue={titleValue}/>
+            <h1>Tiere in der Savanne</h1>
+            <Divider sx={{borderBottomWidth: 5}}/>
+            <div>
+                <div className="savanne-tiere">
+                    <img src={zebraImg}/>
+                    <img src={giraffeImg}/>
+                    <br/>
+                    <img src={gazelleImg}/>
+                    <img src={nashornImg}/>
+                </div>
+            </div>
         </div>
     )
 }
